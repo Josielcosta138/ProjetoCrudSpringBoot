617f4d43803f2bce36cbf7a074ca0870d93e8f17
Josielcosta138 <cjosiel2@gmail.com>
2022-04-14 20:26:55 -0300

    Comentários refeitos

--- springboot-rest-api-sample/src/main/java/br/com/springboot/projeto_Josiel_Mvc/Application.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/projeto_Josiel_Mvc/Application.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  *
  * Spring Boot application starter class
+ * 	SpringApllication.run() Starta aplicação      
+ * Class Application / main / run 
  */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
     	
-        SpringApplication.run(Application.class, args); 
+        SpringApplication.run(Application.class, args); 	//linha principal onde irá rodar o Projeto 	
     }
 }
--- springboot-rest-api-sample/src/main/java/br/com/springboot/projeto_Josiel_Mvc/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/projeto_Josiel_Mvc/controllers/GreetingsController.java
@@ -11,17 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * A sample greetings controller to return greeting text
  *  RestController intercepta todas requisição através da url mapeada
+ *  Navegodar  / celular / tablet / outra máquina / app , depois executa métodos
  */
-@RestController  
+@RestController 
 public class GreetingsController {
     /**
      *
      * @param name the name to greet
      * @return greeting text
      */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)  //acessivel pela URL
+    @RequestMapping(value = "/mostrarNome/{name}", method = RequestMethod.GET)  //parametro acessando a pág pela URL
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
-        return "Projeto Spring Boot API MVC: " + name + "!";
+        return "Projeto Cadastro Spring Boot API MVC: " + name + "!";     
     }
 }
