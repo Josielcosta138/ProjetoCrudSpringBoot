a6b2a0423c5d8c0349d2245260e91d6828c340c5
Josielcosta138 <cjosiel2@gmail.com>
2022-04-13 23:58:12 -0300

    Inicializando Spring Boot Mudando port 8080 -> 8081

--- springboot-rest-api-sample/src/main/java/br/com/springboot/projeto_Josiel_Mvc/Application.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/projeto_Josiel_Mvc/Application.java
@@ -11,6 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
     	
-        SpringApplication.run(Application.class, args); //linha de execução do Projeto Spring Boot	
+        SpringApplication.run(Application.class, args); 
     }
 }
